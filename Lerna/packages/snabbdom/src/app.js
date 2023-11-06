7c7b9c6939254c976c5353eb3294bb6965b45761
PessimistGod <rameshrahul0004@gmail.com>
2023-11-06 12:06:52 +0530

    Generic Code [Test 1]

--- Lerna/packages/snabbdom/src/app.js
+++ Lerna/packages/snabbdom/src/app.js
@@ -7,86 +7,78 @@ import {
 } from "snabbdom";
 
 import { stylesMain } from "./style";
+
 const patch = init([
     classModule,
     styleModule,
     eventListenersModule,
 ]);
 
-let container = document.getElementById("app");
-let count = 0;
-let intervalId = null;
+function createCounterApp(containerId, initialState = 0) {
+    let container = document.getElementById(containerId);
+    let count = initialState;
+    let intervalId = null;
 
-
-
-// Lifecycle hooks
-const lifecycleHooks = {
-    onMount: () => {
-        console.log("Component mounted");
-    },
-    onStateChange: () => {
-        console.log("State changed");
-    },
-};
-
-function updateState() {
-    count++;
-    update();
-}
-
-function startIncrement() {
-    if (intervalId === null) {
-        intervalId = setInterval(() => {
-            count++;
-            update();
-        }, 1000);
+    function updateState() {
+        count++;
+        update();
     }
-}
 
-function stopIncrement() {
-    if (intervalId !== null) {
-        clearInterval(intervalId);
-        intervalId = null;
+    function startIncrement() {
+        if (intervalId === null) {
+            intervalId = setInterval(() => {
+                count++;
+                update();
+            }, 1000);
+        }
     }
-}
 
-function resetCounter() {
-    count = 0;
-    update();
-}
+    function stopIncrement() {
+        if (intervalId !== null) {
+            clearInterval(intervalId);
+            intervalId = null;
+        }
+    }
 
-function view() {
-    return h("div#container", { style: { ...stylesMain.container, ...stylesMain.flexCenter } }, [
-        h("h1", { style: { ...stylesMain.h1Style } }, `${count}`),
-        h("div.child1", { style: { ...stylesMain.flexCenter } }, [
-            h("button", { on: { click: updateState }, style: { ...stylesMain.button } }, "Add"),
-            h("div.child2", { style: { ...stylesMain.divChild2 } }, [
-                h("button", { on: { click: startIncrement }, style: { ...stylesMain.startStopButton } }, "Start"),
-                h("button", { on: { click: stopIncrement }, style: { ...stylesMain.startStopButton } }, "Stop"),
+    function resetCounter() {
+        count = 0;
+        update();
+    }
+
+    function view(count) {
+        return h("div#container", { style: { ...stylesMain.container, ...stylesMain.flexCenter } }, [
+            h("h1", { style: { ...stylesMain.h1Style } }, `${count}`),
+            h("div.child1", { style: { ...stylesMain.flexCenter } }, [
+                h("button", { on: { click: updateState }, style: { ...stylesMain.button } }, "Add"),
+                h("div.child2", { style: { ...stylesMain.divChild2 } }, [
+                    h("button", { on: { click: startIncrement }, style: { ...stylesMain.startStopButton } }, "Start"),
+                    h("button", { on: { click: stopIncrement }, style: { ...stylesMain.startStopButton } }, "Stop"),
+                ]),
+                h("button", { on: { click: resetCounter }, style: { ...stylesMain.resetButton } }, "Reset"),
             ]),
-            h("button", { on: { click: resetCounter }, style: { ...stylesMain.resetButton } }, "Reset"),
-        ]),
-    ]);
+        ]);
+    }
+
+    const initialVnode = view(count);
+
+    function mount() {
+        console.log("Component Mounted");
+        container = patch(container, initialVnode);
+    }
+
+    function update() {
+        console.log("Updating Component");
+        const newVnode = view(count);
+        container = patch(container, newVnode);
+    }
+
+
+    return {
+        mount,
+        update,
+    };
 }
 
-const oldVnode = h("div#container", { style: { ...stylesMain.container, ...stylesMain.flexCenter } }, [
-    h("h1", { style: { ...stylesMain.h1Style } }, `${count}`),
-    h("div.child1", { style: { ...stylesMain.flexCenter } }, [
-        h("button", { on: { click: updateState }, style: { ...stylesMain.button } }, "Add"),
-        h("div.child2", { style: { ...stylesMain.divChild2 } }, [
-            h("button", { on: { click: startIncrement }, style: { ...stylesMain.startStopButton } }, "Start"),
-            h("button", { on: { click: stopIncrement }, style: { ...stylesMain.startStopButton } }, "Stop"),
-        ]),
-        h("button", { on: { click: resetCounter }, style: { ...stylesMain.resetButton } }, "Reset"),
-    ]),
-]);
+const counterApp = createCounterApp("app");
 
-lifecycleHooks.onMount();
-
-container = patch(container, oldVnode);
-
-function update() {
-    lifecycleHooks.onStateChange();
-    const newVnode = view();
-    container = patch(container, newVnode);
-}
+counterApp.mount();
